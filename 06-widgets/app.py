878ff4fbc9bb49b3e045891ca0467da6d26d9bae
Victor Tetteh-Ganoh Ahumah <kofiganoh@gmail.com>
2024-05-01 23:04:29 +0000

    add layouts

--- 06-widgets/app.py
+++ 06-widgets/app.py
@@ -41,3 +41,21 @@ if show_map:
         map_data,
         color="#ffaa0088"
     )
+
+st.divider()
+
+
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+    "first column": [1, 2, 3, 4],
+    "second column": [10, 20, 30, 40]
+    })
+
+option = st.selectbox(
+    "Which number do you like best?",
+     df["first column"],
+    )
+
+"You selected: ", option
