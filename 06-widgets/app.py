ca57b6c5114e0aa25b98f5bb277bc4f0bb464972
Victor Tetteh-Ganoh Ahumah <kofiganoh@gmail.com>
2024-05-01 17:58:39 +0000

    add text input

--- 06-widgets/app.py
+++ 06-widgets/app.py
@@ -3,3 +3,11 @@ import streamlit as st
 
 x = st.slider("x")
 st.write(x, "squared is ", x*x)
+
+st.divider()
+
+st.write("Displaying text input")
+input = st.text_input("Enter your name", key="name")
+
+if input:
+    st.write("Hello ", st.session_state.name)
