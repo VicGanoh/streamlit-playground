49b66a3b0a18c6ce34b5782800bca2d83599d36f
Victor Tetteh-Ganoh Ahumah <kofiganoh@gmail.com>
2024-05-01 18:14:12 +0000

    more on widgets

--- 06-widgets/app.py
+++ 06-widgets/app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 
 x = st.slider("x")
@@ -11,3 +13,31 @@ input = st.text_input("Enter your name", key="name")
 
 if input:
     st.write("Hello ", st.session_state.name)
+    # st.write("Hello", input)
+
+
+st.divider()
+
+st.write("Displaying the use of checkbox")
+
+show_dataframe = st.checkbox("Show dataframe")
+show_map = st.checkbox("Show map")
+
+if show_dataframe:
+    chart_data = pd.DataFrame(
+        np.random.randn(10, 3),
+        columns=["a", "b", "c"]
+    )
+
+    chart_data
+
+if show_map:
+    map_data = pd.DataFrame(
+        np.random.randn(1000, 2) / [50, 50] + [5.614818, -0.205874],
+        columns=["lat", "lon"]
+    )
+
+    st.map(
+        map_data,
+        color="#ffaa0088"
+    )
